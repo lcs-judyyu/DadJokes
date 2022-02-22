5a4f207a84c4505445026ceb9e6bc8f2d3b6e617
Judy <jyu@lcs.on.ca>
2022-02-22 11:41:13 -0500

    Lay out static interface

--- DadJokes/ContentView.swift
+++ DadJokes/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            
+            Text("How do you organize a space party? You planet.")
+                .multilineTextAlignment(.leading)
+                .font(.title2)
+                .padding(30)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.primary, lineWidth: 4)
+                )
+                .padding(10)
+            
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "heart.circle")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(Color.black)
+            })
+            
+            Button(action: {
+           
+            }, label: {
+                Text("Another one!")
+            })
+                .buttonStyle(.bordered)
+            
+            HStack {
+                Text("Favourites")
+                Spacer()
+            }
+            
+            List {
+                Text("Which side of the chicken has more feathers? The outside.")
+                Text("Why did the Clydesdale give the pony a glass of water? Because he was a little horse!")
+                Text("The great thing about stationery shops is they're always in the same place...")
+            }
+            
+            Spacer()
+                        
+        }
+        .navigationTitle("icanhazdadjoke?")
+        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
